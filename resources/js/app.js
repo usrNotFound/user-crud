e741c3a00d9c935b721ea11ae65e5fb5585a7f62
Kamal Chapagain <kamalchapagain01@gmail.com>
2021-11-26 00:42:26 +1000

    install inertiajs and tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+import { render } from 'react-dom'
+
 require('./bootstrap');
+
+InertiaProgress.init();
+
+createInertiaApp({
+    resolve : name => import(`./Pages/${name}`),
+    setup({el, App, props}) {
+        render(<App {...props} />, el);
+    },
+});
